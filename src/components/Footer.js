9e973551dfaa8dcfbd3df4097d996a9c39e93cfc
Thales Bruno <thalesbrunom@gmail.com>
2019-05-17 17:45:12 -0300

    changing some button styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css'
 
 const Footer = () => (
     <footer className="app-footer">
-        <h3>2019 - Made by @thlsbrn</h3>
+        <h3>2019 - Made by <a href="https://github.com/thalesbruno" target="_blank">@thlsbrn</a></h3>
     </footer>
     
 )
